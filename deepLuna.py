8537a0a0a06ab1d7ec78f022868137d3cf8b5a95
Hakanaou <nicolaf2009@live.fr>
2021-09-14 20:59:05 +0200

    Updated version number

--- deepLuna.py
+++ deepLuna.py
@@ -867,7 +867,7 @@ class Informations:
         information.geometry("400x150")
         information.resizable(height=False, width=False)
 
-        self.nom = tk.Label(information, text ="deepLuna v2.2 — 9/10/2021\nDevelopped by Hakanaou\n", justify=tk.CENTER, borderwidth=10)
+        self.nom = tk.Label(information, text ="deepLuna v2.2.1 — 2021/09/14\nDevelopped by Hakanaou\n", justify=tk.CENTER, borderwidth=10)
         self.nom.pack()
 
         self.explanations = tk.Button(information, text="deepLuna GitHub", command=self.callback)
